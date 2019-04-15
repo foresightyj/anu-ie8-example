c70dde39a3c3c03b36774838862cad6f71537a2b
foresightyj <imyuanjian@gmail.com>
2019-04-15 15:32:19 +0800

    with import "es5-shim/es5-sham", ie8 freezes forever

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'es5-shim'; //IE8 ^4.5.10
-// import 'es5-shim/es5-sham'; //IE8 ^4.5.10
+import 'es5-shim/es5-sham'; //IE8 ^4.5.10
 import 'object-create-ie8';//IE8, 我写的库，这样就不用加上es5-sham
 import 'object-defineproperty-ie8';//IE8， 我写的库
 import 'console-polyfill';//IE8下，如果你不打开开发者工具，window下是没有console这个对象的，
